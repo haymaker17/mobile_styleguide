46a1144bdfba0383e6aa0124da2ab84dd5af7e97
olivierb <olivier.bourlon@concur.com>
2015-01-16 17:19:14 +0100

    MOB-21818 - [Android] Forms & fields segments : webservice calls Prerequisite implementation: extraction of an abstractActivity class to handle display & behavior of FormField fields from the existing RequestHeaderActivity class.

--- core/src/com/concur/mobile/core/request/activity/RequestHeaderActivity.java
+++ core/src/com/concur/mobile/core/request/activity/RequestHeaderActivity.java
@@ -2,81 +2,35 @@ package com.concur.mobile.core.request.activity;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
 import java.util.Locale;
-import java.util.Map;
 
-import android.app.ActionBar.LayoutParams;
 import android.app.DatePickerDialog;
-import android.app.DatePickerDialog.OnDateSetListener;
-import android.app.Dialog;
-import android.app.DialogFragment;
-import android.graphics.Color;
 import android.graphics.Typeface;
-import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
-import android.text.InputType;
-import android.text.TextUtils.TruncateAt;
 import android.util.Log;
-import android.util.TypedValue;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.widget.Button;
-import android.widget.DatePicker;
-import android.widget.EditText;
 import android.widget.LinearLayout;
-import android.widget.ListView;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.concur.core.R;
 import com.concur.mobile.core.ConcurCore;
-import com.concur.mobile.core.activity.BaseActivity;
-import com.concur.mobile.core.request.activity.RequestListActivity.ListAdapterRowClickListener;
+import com.concur.mobile.core.activity.AbstractConnectFormFieldActivity;
 import com.concur.mobile.core.util.Const;
-import com.concur.mobile.platform.common.formfield.ConnectForm;
-import com.concur.mobile.platform.common.formfield.ConnectFormField;
 import com.concur.mobile.platform.common.formfield.ConnectFormFieldsCache;
-import com.concur.mobile.platform.common.formfield.IFormField.AccessType;
-import com.concur.mobile.platform.common.formfield.IFormField.ControlType;
-import com.concur.mobile.platform.common.formfield.IFormField.DataType;
 import com.concur.mobile.platform.request.RequestListCache;
 import com.concur.mobile.platform.request.dto.RequestDTO;
 import com.concur.mobile.platform.util.Parse;
 
-public class RequestHeaderActivity extends BaseActivity implements OnClickListener  {
+public class RequestHeaderActivity extends AbstractConnectFormFieldActivity implements OnClickListener  {
+
+    private static final String CLS_TAG = RequestListActivity.class.getSimpleName();
 
 	private RequestListCache requestListCache = null;
 	private ConnectFormFieldsCache formFieldsCache = null;
-
 	private RequestDTO tr;
-	private ConnectForm form;
-
-	private static final String CLS_TAG = RequestListActivity.class.getSimpleName();
-
-	private SimpleDateFormat dateFormatter;
 	private Locale loc;
-
-	private DatePickerDialog fromDatePickerDialog;
-
-	private Map<Integer, String> views = new HashMap(); // view ID, filed NAME
-	private int viewID = 0;
-
-	private List dateViews = new ArrayList<Integer>();
-	private List datePickerDialogs = new ArrayList<DatePickerDialog>();
-
-	private static enum DisplayType {
-		TEXTFIELD,		//RequestID, Name, Status
-		TEXTAREA,		//Purpose, Comment
-		DATEFIELD,		//startDate, endate
-		MONEYFIELD;		//totalAmount&currency
-	}
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -110,7 +64,6 @@ public class RequestHeaderActivity extends BaseActivity implements OnClickListen
 	}
 
 	private void setUI(){
-
 		final TextView name = (TextView) findViewById(R.id.requestHeaderName);
 		name.setText(tr.getName());
 		name.setTypeface(Typeface.DEFAULT_BOLD);
@@ -119,216 +72,13 @@ public class RequestHeaderActivity extends BaseActivity implements OnClickListen
 		this.setDisplayFields(requestHeaderFields);
 	}
 
-
-	private void setDisplayFields(LinearLayout requestHeaderFields){
-
-		LinearLayout ffLayout = requestHeaderFields;
-
-		List<ConnectFormField> formfields = form.getFormFields();    	
-		Collections.sort(formfields);
-		
-		LinearLayout.LayoutParams llp;
-
-		for(ConnectFormField ff : formfields){
-
-			DataType dataType = ff.getDataType();
-			ControlType controlType = ff.getControlType();
-			AccessType accesType = ff.getAccessType();
-
-
-			DisplayType displayType = null;
-
-			switch(dataType){
-			case BOOLEAN:
-				break;
-			case CHAR:
-				break;
-			case CONNECTED_LIST:
-				break;
-			case CURRENCY:
-				break;
-			case EXPENSE_TYPE:
-				break;
-			case INTEGER:
-				break;
-			case LIST:
-				break;
-			case LOCATION:
-				break;
-			case MONEY:
-				displayType = DisplayType.MONEYFIELD;
-				break;
-			case NUMERIC:
-				break;
-			case TIMESTAMP:
-				displayType = DisplayType.DATEFIELD;
-				break;
-			case UNSPECIFED:
-				break;
-			case VARCHAR:
-				switch(controlType){
-				case EDIT:
-					displayType = DisplayType.TEXTFIELD;
-					break;
-				case TEXT_AREA:
-					displayType = DisplayType.TEXTAREA;
-					break;
-				default :
-					break;
-				}
-				break;
-			default:
-				displayType = null;
-				break;
-			}
-
-
-			if(displayType != null)
-
-				switch(displayType){
-				case TEXTFIELD:
-
-					TextView textField;
-					if(accesType == accesType.RO)
-						textField = new TextView(this);
-					else
-						textField = new EditText(this);
-
-					ffLayout.addView(getTextView_FielddName(getLabelFromFieldName(ff.getName())));
-					textField.setText(getValueFromFieldName(ff.getName()));
-					textField.setMaxLines(1);
-					textField.setSingleLine(true); textField.setEllipsize(TruncateAt.END); //ellipses
-					textField.setTextSize(TypedValue.COMPLEX_UNIT_SP, 15);
-					ffLayout.addView(textField);
-
-					llp = new LinearLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
-					if(accesType == accesType.RO) llp.setMargins(30, 0, 0, 0); // llp.setMargins(left, top, right, bottom);
-					textField.setLayoutParams(llp);
-					
-					textField.setId(viewID);
-					views.put(viewID, ff.getName()); viewID++;
-
-					addWhiteSpace(ffLayout);
-					if(ff.isLineSeparator()) addSeparator(ffLayout);
-
-					break;
-
-				case TEXTAREA:
-
-					TextView textArea;
-					if(accesType == accesType.RO)
-						textArea = new TextView(this);
-					else
-						textArea = new EditText(this);
-
-					ffLayout.addView(getTextView_FielddName(getLabelFromFieldName(ff.getName())));
-
-					textArea.setText(getValueFromFieldName(ff.getName()));
-					textArea.setLines(5);
-					textArea.setMaxLines(5);
-					textArea.setTextSize(TypedValue.COMPLEX_UNIT_SP, 15);
-					ffLayout.addView(textArea);
-
-					llp = new LinearLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
-					if(accesType == accesType.RO) llp.setMargins(30, 0, 0, 0); // llp.setMargins(left, top, right, bottom);
-					textArea.setLayoutParams(llp);
-					
-					textArea.setId(viewID);
-					views.put(viewID, ff.getName()); viewID++;
-
-					addWhiteSpace(ffLayout);
-					if(ff.isLineSeparator()) addSeparator(ffLayout);
-
-					break;
-
-				case DATEFIELD:
-
-					final TextView 
-					fromDateEtxt = new TextView(this);
-
-					ffLayout.addView(getTextView_FielddName(getLabelFromFieldName(ff.getName())));
-
-					fromDateEtxt.setText(getValueFromFieldName(ff.getName()));
-					fromDateEtxt.setInputType(InputType.TYPE_NULL);
-					fromDateEtxt.setTextSize(TypedValue.COMPLEX_UNIT_SP, 15);
-					fromDateEtxt.requestFocus();
-
-					//ADD LISTENER for setting date
-					if(!(accesType == accesType.RO)){
-						fromDateEtxt.setOnClickListener((OnClickListener) this);
-						Calendar newCalendar = Calendar.getInstance();
-
-						OnDateSetListener inDateListener = new OnDateSetListener(){
-
-							public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
-								Calendar newDate = Calendar.getInstance();
-								newDate.set(year, monthOfYear, dayOfMonth);
-								fromDateEtxt.setText(dateFormatter.format(newDate.getTime()));
-							}
-						};
-
-						DatePickerDialog fromDatePickerDialog = new DatePickerDialog(this, inDateListener, 
-								newCalendar.get(Calendar.YEAR), newCalendar.get(Calendar.MONTH), newCalendar.get(Calendar.DAY_OF_MONTH));
-
-						dateViews.add(viewID);
-						datePickerDialogs.add(fromDatePickerDialog);
-					}
-
-					fromDateEtxt.setId(viewID);
-					llp = new LinearLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
-					llp.setMargins(30, 0, 0, 0); // llp.setMargins(left, top, right, bottom);
-					fromDateEtxt.setLayoutParams(llp);
-
-					views.put(viewID, ff.getName()); viewID++;
-
-					ffLayout.addView(fromDateEtxt);
-
-					addWhiteSpace(ffLayout);
-					if(ff.isLineSeparator()) addSeparator(ffLayout);
-
-					break;
-
-				case MONEYFIELD:
-
-					ffLayout.addView(getTextView_FielddName(getLabelFromFieldName(ff.getName())));
-
-					TextView moneyField = new TextView(this);
-					moneyField.setText(getValueFromFieldName("CurrencyName") + " " + getValueFromFieldName(ff.getName()));
-					moneyField.setMaxLines(1);
-					moneyField.setSingleLine(true); moneyField.setEllipsize(TruncateAt.END); //ellipses
-					moneyField.setTextSize(TypedValue.COMPLEX_UNIT_SP, 15);
-					ffLayout.addView(moneyField);
-
-					moneyField.setId(viewID);
-					views.put(viewID, ff.getName()); viewID++;
-					
-					llp = new LinearLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
-					if(accesType == accesType.RO) llp.setMargins(30, 0, 0, 0); // llp.setMargins(left, top, right, bottom);
-					moneyField.setLayoutParams(llp);
-
-					addWhiteSpace(ffLayout);
-					if(ff.isLineSeparator()) addSeparator(ffLayout);
-
-					break;
-
-				default:
-					displayType = null;
-					break;
-				}
-		}
-	}
-
-
 	@Override
 	public void onClick(View view) {
-
-		for(int i=0; i<dateViews.size(); i++){
-			if((Integer)dateViews.get(i) == view.getId()){
-				((DatePickerDialog) datePickerDialogs.get(i)).show();
+		for(int i = 0; i < getDateViews().size(); i++){
+			if((Integer)getDateViews().get(i) == view.getId()){
+				((DatePickerDialog) getDatePickerDialogs().get(i)).show();
 			}
 		}
-
-
 	}
 
 	@Override
@@ -344,10 +94,10 @@ public class RequestHeaderActivity extends BaseActivity implements OnClickListen
 	@Override
 	public void onBackPressed() {
 		super.onBackPressed();
-
 	}
 
-	public String getLabelFromFieldName(String fieldName){
+    @Override
+	protected String getLabelFromFieldName(String fieldName){
 
 		if(fieldName.equals("Name")){
 			return this.getResources().getString(R.string.tr_name);
@@ -376,6 +126,7 @@ public class RequestHeaderActivity extends BaseActivity implements OnClickListen
 		else return "";
 	}
 
+    @Override
 	public String getValueFromFieldName(String fieldName){
 
 		if(fieldName.equals("Name")){
@@ -383,11 +134,11 @@ public class RequestHeaderActivity extends BaseActivity implements OnClickListen
 			return tr.getName();
 		}
 		else if(fieldName.equals("StartDate")){
-			if(tr.getStartDate() == null || tr.getStartDate().equals("")) return "";
+			if(tr.getStartDate() == null) return "";
 			return dateFormatter.format(tr.getStartDate());
 		}
 		else if(fieldName.equals("EndDate")){
-			if(tr.getStartDate() == null || tr.getEndDate().equals("")) return "";
+			if(tr.getStartDate() == null) return "";
 			return dateFormatter.format(tr.getEndDate());
 		}
 		else if(fieldName.equals("Purpose")){
@@ -411,8 +162,8 @@ public class RequestHeaderActivity extends BaseActivity implements OnClickListen
 		else return "";
 	}
 
-	public void setValueFromFieldName(String fieldName, String value){
-
+    @Override
+    protected void setValueFromFieldName(String fieldName, String value){
 		if(fieldName.equals("Name"))
 			tr.setName(value);
 		else if(fieldName.equals("StartDate"))
@@ -442,44 +193,4 @@ public class RequestHeaderActivity extends BaseActivity implements OnClickListen
 		}
 		return null;
 	}
-
-
-	private void addWhiteSpace(LinearLayout mainLayout){
-
-		View view = new View(this);
-		ColorDrawable backgroundColor = new ColorDrawable(this.getResources().getColor(R.color.White));
-		view.setBackground(backgroundColor);
-
-		int viewWidth = LayoutParams.MATCH_PARENT; //FILL_PARENT = MATCH_PARENT
-		int viewHeight = 50;
-		mainLayout.addView(view, viewWidth, viewHeight);
-	}
-	
-	private void addSeparator(LinearLayout mainLayout){
-
-		View view = new View(this);
-		ColorDrawable backgroundColor = new ColorDrawable(this.getResources().getColor(R.color.ListDivider));
-		view.setBackground(backgroundColor);
-
-		int viewWidth = LayoutParams.MATCH_PARENT; //FILL_PARENT = MATCH_PARENT
-		int viewHeight = 15;
-		mainLayout.addView(view, viewWidth, viewHeight);
-	}
-
-	private TextView getTextView_FielddName(String text){
-
-		TextView textView = new TextView(this);
-		textView.setText(text);
-		textView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 13);
-		textView.setTextColor(Color.parseColor("#666666"));
-
-		LinearLayout.LayoutParams llp = new LinearLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
-		llp.setMargins(30, 0, 0, 0); // llp.setMargins(left, top, right, bottom);
-		textView.setLayoutParams(llp);
-
-		return textView;
-	}
-
-
-
 }
