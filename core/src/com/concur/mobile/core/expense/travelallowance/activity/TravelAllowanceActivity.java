66df49a44eeb2ed4324d037fae9c56a85777cff7
Michael Becherer <michael.becherer@sap.com>
2015-07-01 13:52:46 +0200

    Trying out CalendarPickerDialogV1
    Change-Id: Ie6469667006b2f052a48a1c6a4c1872faaa05e36

--- core/src/com/concur/mobile/core/expense/travelallowance/activity/TravelAllowanceActivity.java
+++ core/src/com/concur/mobile/core/expense/travelallowance/activity/TravelAllowanceActivity.java
@@ -1,6 +1,7 @@
 package com.concur.mobile.core.expense.travelallowance.activity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
@@ -9,6 +10,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.concur.core.R;
 import com.concur.mobile.core.ConcurCore;
@@ -20,7 +22,11 @@ import com.concur.mobile.core.expense.travelallowance.datamodel.FixedTravelAllow
 import com.concur.mobile.core.expense.travelallowance.fragment.FixedTravelAllowanceListFragment;
 import com.concur.mobile.core.expense.travelallowance.fragment.IFragmentCallback;
 import com.concur.mobile.core.expense.travelallowance.fragment.TravelAllowanceItineraryListFragment;
+import com.concur.mobile.core.travel.hotel.activity.HotelSearch;
 import com.concur.mobile.core.util.Const;
+import com.concur.mobile.platform.ui.common.widget.CalendarPickerDialogV1;
+
+import java.util.Calendar;
 
 /**
  * Created by D049515 on 15.06.2015.
@@ -31,6 +37,12 @@ public class TravelAllowanceActivity extends AppCompatActivity
     private static final String CLASS_TAG = TravelAllowanceActivity.class.getSimpleName();
 
     private static final int REQUEST_CODE_FIXED_TRAVEL_ALLOWANCE_DETAILS = 0x01;
+
+    private static final int CHECK_IN_DATE_DIALOG = 0;
+    private static final int CHECK_OUT_DATE_DIALOG = 1;
+    private static final String TAG_CALENDAR_DIALOG_FRAGMENT =
+            HotelSearch.class.getSimpleName() + ".calendar.dialog.fragment";
+    private CalendarPickerDialogV1 calendarDialog;
 
     /**
      * Contains the key identifying the source of this report key. Should be one of
@@ -109,6 +121,62 @@ public class TravelAllowanceActivity extends AppCompatActivity
             tabLayout.setupWithViewPager(pager);
         }
 
+        if (expenseReportKeySource != Const.EXTRA_EXPENSE_REPORT_SOURCE_APPROVAL) {
+            View checkOutDateView = findViewById(R.id.hotel_search_check_out);
+            View checkInDateView = findViewById(R.id.hotel_search_label_checkin);
+
+            // Hook up the handlers
+            checkInDateView.setOnClickListener(new View.OnClickListener() {
+
+                @Override public void onClick(View v) {
+                    showCalendarDialog(CHECK_IN_DATE_DIALOG);
+                }
+            });
+            checkOutDateView.setOnClickListener(new View.OnClickListener() {
+
+                @Override public void onClick(View v) {
+                    showCalendarDialog(CHECK_OUT_DATE_DIALOG);
+                }
+            });
+        }
+
+    }
+
+    private void showCalendarDialog(int id) {
+        Bundle bundle;
+
+        //Temporary for testing
+        //TODO: Replace
+        Calendar checkInDate = Calendar.getInstance();
+        Calendar checkOutDate = Calendar.getInstance();
+        boolean isCheckin;
+
+        calendarDialog = new CalendarPickerDialogV1();
+        bundle = new Bundle();
+        switch (id) {
+            case CHECK_IN_DATE_DIALOG: {
+                bundle.putInt(CalendarPickerDialogV1.KEY_INITIAL_YEAR, checkInDate.get(Calendar.YEAR));
+                bundle.putInt(CalendarPickerDialogV1.KEY_INITIAL_MONTH, checkInDate.get(Calendar.MONTH));
+                bundle.putInt(CalendarPickerDialogV1.KEY_INITIAL_DAY, checkInDate.get(Calendar.DAY_OF_MONTH));
+                bundle.putInt(CalendarPickerDialogV1.KEY_TEXT_COLOR, Color.parseColor("#a5a5a5"));
+                isCheckin = true;
+                break;
+            }
+            case CHECK_OUT_DATE_DIALOG: {
+                bundle.putInt(CalendarPickerDialogV1.KEY_INITIAL_YEAR, checkOutDate.get(Calendar.YEAR));
+                bundle.putInt(CalendarPickerDialogV1.KEY_INITIAL_MONTH, checkOutDate.get(Calendar.MONTH));
+                bundle.putInt(CalendarPickerDialogV1.KEY_INITIAL_DAY, checkOutDate.get(Calendar.DAY_OF_MONTH));
+                bundle.putInt(CalendarPickerDialogV1.KEY_TEXT_COLOR, Color.parseColor("#a5a5a5"));
+
+                isCheckin = false;
+                break;
+            }
+        }
+
+        //DialogId = id;
+        calendarDialog.setArguments(bundle);
+        //calendarDialog.setOnDateSetListener(new HotelDateSetListener(id, isCheckin));
+        calendarDialog.show(getFragmentManager(), TAG_CALENDAR_DIALOG_FRAGMENT);
     }
 
     @Override
