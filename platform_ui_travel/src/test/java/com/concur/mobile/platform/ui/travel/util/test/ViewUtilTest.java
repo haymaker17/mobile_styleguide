d4992e784e85c20cf2afe54367ff9bcd405e19b8
Ratan Korlam <ratan.korlam@concur.com>
2015-07-28 18:07:55 +0100

    MOB-23291 - Unit tests

--- platform_ui_travel/src/test/java/com/concur/mobile/platform/ui/travel/util/test/ViewUtilTest.java
+++ platform_ui_travel/src/test/java/com/concur/mobile/platform/ui/travel/util/test/ViewUtilTest.java
@@ -2,6 +2,7 @@ package com.concur.mobile.platform.ui.travel.util.test;
 
 import android.content.Context;
 import android.widget.TextView;
+import com.concur.mobile.platform.travel.search.hotel.RuleEnforcementLevel;
 import com.concur.mobile.platform.ui.travel.util.ViewUtil;
 import org.junit.Assert;
 import org.junit.Test;
@@ -22,5 +23,57 @@ import org.robolectric.annotation.Config;
         TextView txtView = new TextView(context);
         ViewUtil.showGDSName(context, txtView, "Sabre");
         Assert.assertEquals("GDS Name set in TextView is in-correct", "(Sabre)", txtView.getText());
+    }
+
+    @Test public void testGetRuleEnforcementLevelAsString() {
+        // test null - default returns NONE
+        Assert.assertEquals("NONE", ViewUtil.getRuleEnforcementLevelAsString(null));
+
+        //test NONE - less than 0 or 100
+        Assert.assertEquals("NONE", ViewUtil.getRuleEnforcementLevelAsString(9));
+        Assert.assertEquals("NONE", ViewUtil.getRuleEnforcementLevelAsString(100));
+
+        // test WARNING - 10 or 20
+        Assert.assertEquals("WARNING", ViewUtil.getRuleEnforcementLevelAsString(10));
+        Assert.assertEquals("WARNING", ViewUtil.getRuleEnforcementLevelAsString(20));
+
+        // test ERROR - 25 or 30
+        Assert.assertEquals("ERROR", ViewUtil.getRuleEnforcementLevelAsString(25));
+        Assert.assertEquals("ERROR", ViewUtil.getRuleEnforcementLevelAsString(30));
+
+        // test INACTIVE - 40
+        Assert.assertEquals("INACTIVE", ViewUtil.getRuleEnforcementLevelAsString(40));
+
+        // test HIDE - 50
+        Assert.assertEquals("HIDE", ViewUtil.getRuleEnforcementLevelAsString(50));
+    }
+
+    @Test public void testGetRuleEnforcementLevel() {
+        // test NONE - less than 0 or 100
+        RuleEnforcementLevel ruleEnforcementLevel = ViewUtil.getRuleEnforcementLevel(9);
+        Assert.assertEquals(RuleEnforcementLevel.NONE, ruleEnforcementLevel);
+        ruleEnforcementLevel = ViewUtil.getRuleEnforcementLevel(100);
+        Assert.assertEquals(RuleEnforcementLevel.NONE, ruleEnforcementLevel);
+
+        // test WARNING - 10 or 20
+        ruleEnforcementLevel = ViewUtil.getRuleEnforcementLevel(10);
+        Assert.assertEquals(RuleEnforcementLevel.WARNING, ruleEnforcementLevel);
+        ruleEnforcementLevel = ViewUtil.getRuleEnforcementLevel(20);
+        Assert.assertEquals(RuleEnforcementLevel.WARNING, ruleEnforcementLevel);
+
+        // test ERROR - 25 or 30
+        ruleEnforcementLevel = ViewUtil.getRuleEnforcementLevel(25);
+        Assert.assertEquals(RuleEnforcementLevel.ERROR, ruleEnforcementLevel);
+        ruleEnforcementLevel = ViewUtil.getRuleEnforcementLevel(30);
+        Assert.assertEquals(RuleEnforcementLevel.ERROR, ruleEnforcementLevel);
+
+        // test INACTIVE - 40
+        ruleEnforcementLevel = ViewUtil.getRuleEnforcementLevel(40);
+        Assert.assertEquals(RuleEnforcementLevel.INACTIVE, ruleEnforcementLevel);
+
+        // test HIDE - 50
+        ruleEnforcementLevel = ViewUtil.getRuleEnforcementLevel(50);
+        Assert.assertEquals(RuleEnforcementLevel.HIDE, ruleEnforcementLevel);
+
     }
 }
