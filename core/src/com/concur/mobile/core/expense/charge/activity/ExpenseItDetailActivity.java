503742fa42e872a708ddbf890f57091442e16b2e
Elliott Jacobsen-Watts <Elliott.Jacobsen-Watts@concur.com>
2015-07-31 15:19:19 -0700

    Merged expenseit branch with Harold's current changes.

--- core/src/com/concur/mobile/core/expense/charge/activity/ExpenseItDetailActivity.java
+++ core/src/com/concur/mobile/core/expense/charge/activity/ExpenseItDetailActivity.java
@@ -1,50 +1,59 @@
 package com.concur.mobile.core.expense.charge.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.FragmentManager;
 
 import com.concur.core.R;
 import com.concur.mobile.core.activity.BaseActivity;
+import com.concur.mobile.core.activity.ReceiptView;
+import com.concur.mobile.core.expense.charge.data.ExpenseItItem;
 import com.concur.mobile.core.expense.fragment.ExpenseItDetailActivityFragment;
-import com.concur.mobile.platform.ui.common.fragment.PlatformFragment;
+import com.concur.mobile.core.util.Const;
+import com.concur.mobile.platform.expenseit.ExpenseItReceipt;
 
 /**
  * @author Elliott Jacobsen-Watts
  */
-public class ExpenseItDetailActivity extends BaseActivity {
+public class ExpenseItDetailActivity extends BaseActivity implements ExpenseItDetailActivityFragment.ExpenseItDetailsViewReceiptCallback {
 
-    public static final String FRAGMENT_EXPENSEIT_DETAIL = "FRAGMENT_EXPENSEIT_DETAIL";
+    private static final String FRAGMENT_EXPENSEIT_DETAIL = "FRAGMENT_EXPENSEIT_DETAIL";
 
     public static final String EXPENSEIT_ITEM_KEY = "EXPENSEIT_ITEM_KEY";
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        buildView();
+     public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
     }
 
-    public void buildView() {
-
-        // set some kind of base layout that the fragment will sit on.
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.expense_detail);
 
-        // build the fragment
-        FragmentManager fm = getSupportFragmentManager();
-        PlatformFragment exItDetailsFragment =
-                (PlatformFragment) fm.findFragmentByTag(FRAGMENT_EXPENSEIT_DETAIL);
-        if (exItDetailsFragment == null) {
-            exItDetailsFragment = new ExpenseItDetailActivityFragment();
-            fm.beginTransaction()
-                    .replace(R.id.container, exItDetailsFragment, FRAGMENT_EXPENSEIT_DETAIL)
-                    .commit();
+        ExpenseItItem item = new ExpenseItItem(new ExpenseItReceipt());
+        if (getIntent().getExtras() != null && getIntent().getExtras().containsKey(ExpenseItDetailActivity.EXPENSEIT_ITEM_KEY)) {
+            item = (ExpenseItItem) getIntent().getExtras().getSerializable(ExpenseItDetailActivity.EXPENSEIT_ITEM_KEY);
         }
 
-        // configure header
+        if (getSupportFragmentManager().findFragmentByTag(FRAGMENT_EXPENSEIT_DETAIL) == null) {
+            ExpenseItDetailActivityFragment frag = ExpenseItDetailActivityFragment.newInstance(item);
+            getSupportFragmentManager().beginTransaction()
+                .add(R.id.container, frag, FRAGMENT_EXPENSEIT_DETAIL)
+                .commit();
+        }
+
         configureViewHeader();
     }
 
     protected void configureViewHeader() {
         String title = getText(R.string.quick_expense_title).toString();
         getSupportActionBar().setTitle(title);
+    }
+
+    @Override
+    public void initializeViewReceipt(long receiptId) {
+        Intent intent = new Intent(this, ReceiptView.class);
+        intent.putExtra(Const.EXTRA_EXPENSE_IT_RECEIPT_ID, receiptId);
+        startActivity(intent);
     }
 }
